bc8666c15f7ea24e80a0310fe269c5b966716e9a
sviatoslav <bugatis15@gmail.com>
2019-11-07 12:14:38 +0200

    fix migration

--- src/Database/Migrations/2016_02_09_132440_create_permission_team_table.php
+++ src/Database/Migrations/2016_02_09_132440_create_permission_team_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePermissionUserTable extends Migration
+class CreatePermissionTeamTable extends Migration
 {
     /**
      * Run the migrations.
